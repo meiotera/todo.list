6c5641f0c57b35ed553932abe0051af995dbb698
Renan Ferreira <renanfc.re@gmail.com>
2023-11-08 08:10:48 -0300

    import de controllers e correção de app.set(view engine)

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const hbs = exphbs.create({
   partialsDir: ["views/partials"],
 });
-app.engine("handlwbars", hbs.engine);
-app.set("vews engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(
   express.urlencoded({
@@ -16,5 +16,11 @@ app.use(
 );
 app.use(express.json());
 app.use(express.static("public"));
+
+// import Controllers
+const TodoController = require("./controllers/TodoController");
+
+// app.use("/")
+app.get("/", TodoController.showTodo);
 
 app.listen(3000);
