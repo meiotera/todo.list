964bc0002bfa6ce620d8f55cbbc67d523b4c7164
Renan Ferreira <renanfc.re@gmail.com>
2023-11-09 08:36:38 -0300

    Método static de post Todo configurado

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,5 +1,20 @@
+const TodoModel = require("../models/TodoModel");
+
 module.exports = class TodoController {
   static showTodo(req, res) {
     res.render("partials/todo");
+  }
+
+  static async createTodoPost(req, res) {
+    const todo = {
+      title: req.body.title,
+    };
+
+    try {
+      await TodoModel.create(todo);
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
