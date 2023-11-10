7b146c301f6abea91bd4979dfcbe71dc874b3689
Renan Ferreira <renanfc.re@gmail.com>
2023-11-10 08:06:33 -0300

    TodoController - tratamento de dados vindos do BD realizados no método showTodo

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,11 +1,35 @@
 const TodoModel = require("../models/TodoModel");
 
 module.exports = class TodoController {
-  static showTodo(req, res) {
-    res.render("partials/todo");
+  static async showTodo(req, res) {
+    const todoData = await TodoModel.findAll({
+      attributes: ["title", "concluded", "createdAt"],
+    });
+
+    const resultData = todoData.map((data) => {
+      const todo = data.dataValues;
+
+      const ano = todo.createdAt.getFullYear();
+      const dia = todo.createdAt.getDate();
+      const mes = todo.createdAt.getMonth();
+      const dataDeCriacao = `${dia}/${mes}/${ano}`;
+    
+      return {
+        title: todo.title,
+        concluded: todo.concluded,
+        dataDeCriacao,
+      };
+    });
+
+    res.render("partials/todo", { resultData });
   }
 
   static async createTodoPost(req, res) {
+    if (req.body.title === "") {
+      res.redirect("/");
+      return
+    }
+
     const todo = {
       title: req.body.title,
     };
