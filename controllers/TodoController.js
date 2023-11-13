ffa17bbe1a2ce7835733a7cd609204a5fffe5416
Renan Ferreira <renanfc.re@gmail.com>
2023-11-13 08:44:22 -0300

    Função concluded adicionada para marcar como tarefa concluida, id adicionada na showTodo

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -3,11 +3,11 @@ const TodoModel = require("../models/TodoModel");
 module.exports = class TodoController {
   static async showTodo(req, res) {
     const todoData = await TodoModel.findAll({
-      attributes: ["title", "concluded", "createdAt"],
+      attributes: ["title", "concluded", "createdAt", "id"],
     });
 
     const resultData = todoData.map((data) => {
-      const todo = data.dataValues;
+      let todo = data.dataValues;
 
       const ano = todo.createdAt.getFullYear();
       const dia = todo.createdAt.getDate();
@@ -15,12 +15,14 @@ module.exports = class TodoController {
       const dataDeCriacao = `${dia}/${mes}/${ano}`;
 
       return {
+        id: todo.id,
         title: todo.title,
         concluded: todo.concluded,
         dataDeCriacao,
       };
     });
 
+    console.log(resultData)
     res.render("partials/todo", { resultData });
   }
 
@@ -33,10 +35,29 @@ module.exports = class TodoController {
 
     const todo = {
       title: req.body.title,
+      concluded: false,
     };
 
     try {
       await TodoModel.create(todo);
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async concluded(req, res) {
+    const id = req.body.id;
+
+    const todos = {
+      concluded: true,
+    };
+
+    console.log(todos);
+    try {
+      await TodoModel.update(todos, { where: { id: id } });
+
+      req.flash("message", "Concluido");
       res.redirect("/");
     } catch (error) {
       console.log(error);
