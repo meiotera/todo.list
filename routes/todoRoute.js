5d3c2e5eb8707810719133b00c12e9a16145f430
Renan Ferreira <renanfc.re@gmail.com>
2023-11-13 08:45:30 -0300

    router concluded adionada

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -4,5 +4,6 @@ const TodoController = require("../controllers/TodoController");
 
 // router.get("/add", TodoController.createTodo);
 router.post("/add", TodoController.createTodoPost);
+router.post("/concluido", TodoController.concluded);
 
 module.exports = router;
